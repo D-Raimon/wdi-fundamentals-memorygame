fba72cb3fb0497601f9f56895c78b8d18190438c
David Raimon <draimon@Davids-MacBook-Pro.local>
2019-07-25 17:56:16 -0400

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,11 @@
+var cardOne = "queen";
+
+var cardTwo = "king";
+
+var cardThree = "king";
+
+var cardFour = "queen";
+
+console.log("User flipped " + cardThree);
+
+console.log("User flipped " + cardOne);
