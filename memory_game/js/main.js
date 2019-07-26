8805a2d4aa9ee7ba4794fe04ab1a845d7327a29e
David Raimon <draimon@Davids-MacBook-Pro.local>
2019-07-26 12:02:53 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,18 +1,21 @@
-var cards = ["queen","queen","king","king"];
+var cards = ["queen", "queen", "king", "king"];
 
 var cardsInPlay = [];
 
-var cardOne = "queen";
-cardsInPlay.push("cardOne");
-console.log("User flipped queen");
-
-var cardTwo = "king";
-cardsInPlay.push("cardTwo");
-console.log("User flipped king");
-
-if ((cardsInPlay.length === 2 ) && (cardsInPlay[0] === cardsInPlay[1])) {
-		alert("You found a match!");
+function checkForMatch() {
+	if (cardsInPlay.length === 2)
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+		  console.log("You found a match!");
+		} else {
+		  console.log("Sorry, try again.");
+	}   
 }
-	else {
-		alert("Sorry, try again");
-	}
+
+function flipCard(cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+    checkForMatch();
+}
+
+flipCard(0);
+flipCard(2);
